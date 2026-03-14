9dd883775663a6b42d9b062c2f0685a13e0f53ba
23228cb-colab <23228cb@hvhs.school.nz>
2026-03-14 22:30:45 +1300

    need end + controls

--- grass.js
+++ grass.js
@@ -1,8 +1,14 @@
-// setup()
-/*******************************************************/
+//variables
 	let timer = 0;
 	let grassCount =0;
-	
+//image preload
+function preload() {
+  mowerSpriteIMG = loadImage('/assets/lawnMower.png');
+
+}	
+
+// setup()
+/*******************************************************/
 function setup() {
 	//setup
 	console.log("setup: ");
@@ -12,99 +18,101 @@ function setup() {
 	grassGroup = new Group();
 	frameRate(60);
 	
-	
-	
-	//create a line of 10 sprites across the top of the screen
+	//grass spawn
 	for (var i = 0; i < 38; i++) {
-		spriteColor = color('lime');
+		spriteColor = color('#17c93d');
   	for (var x = 0; x < 44; x++) {
   		var grass = new Sprite(x*15+30, i*15+66, 15, 15, 'k' );
 		grass.color = spriteColor;
     	grassGroup.add(grass);
 }
-}
-//sprites
+};
+
+//wall sprites
 	wallLH  = new Sprite(0, 340, 8, 600, 'k');
 	wallRH  = new Sprite(700, 340, 8, 605, 'k');
 	wallTop = new Sprite(400, 40, 800, 8, 'k');
 	wallBot = new Sprite(400, 645, 800, 8, 'k');
-    mower = new Sprite(45, 535, 60, 45, 'd' );
-	
-	mower.color = "Red";
-	mower.drag = 1;
-	mower.friction = 10;
-	mower.drag = 10;
-	mower.bounciness = -10;
 	wallBot.color = 'black';
 	wallLH.color = 'black';
 	wallRH.color = 'black';
 	wallTop.color = 'black';
+
+//mower sprite code 
+	mower = new Sprite(45, 535, 60, 45, 'd' );
+	mower.image = (mowerSpriteIMG);
+	mower.friction = 20;
 
 //grass collision
 	grassGroup.collides(mower, grassCutFunc );
 	
 }
 	
-
 //grass delete function
 	function grassCutFunc(_ssss, _mower){
 		_ssss.remove();
 	}
-
 
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
 	background("green");
-	text(timer,350,20);
-	textSize(20,20);
+	//timer
+	text(timer,400,20);
+	textSize(20);
 	grassCount = grassGroup.length;
-	console.log(grassGroup.length);
+	
+	//grass left
+	let grassPersent = grassCount/1637*100;
+	grassPersent=Math.round(grassPersent*100)/100;
+	text(grassPersent,300,20);
+	
 
-	if(grassGroup.length=0){
+	//time stop
+	if(grassCount>0){
 	timer=frameCount/60;
 	timer=Math.round(timer*1000)/1000;
-	console.log(timer);
+	
 };
 	
     //mower left/right
 if (kb.pressing('a')) {
-	mower.rotation = 180
-	mower.vel.x = -2;
+	mower.rotation = 270;
+	mower.vel.x = -6;
 };
 
 if (kb.pressing ('d')) {
-	mower.rotation = 0
-	mower.vel.x = 2;
+	mower.rotation = 90;
+	mower.vel.x = 6;
 
 };
 
 if (kb.released('a')){
-	mower.rotation = 180
+	mower.rotation = 180;
 	mower.vel.x = 0;
-	mower.rotationSpeed = 0
+	mower.rotationSpeed = 0;
 }
 
 else if (kb.released('d')){
-	mower.rotation = 0
+	mower.rotation = 0;
 	mower.vel.x = 0;
-	mower.rotationSpeed = 0
+	mower.rotationSpeed = 0;
 };
 
 //mower Up/down
 if (kb.pressing('s')){
-	mower.vel.y = 2;
-	mower.rotation = 90;
+	mower.vel.y = 6;
+	mower.rotation =180;
 }
 else if (kb.released('s')){
 	mower.vel.y = 0;
-	mower.rotationSpeed = 0
+	mower.rotationSpeed = 0;
 };
 
 if (kb.pressing('w')){
-	mower.vel.y = -2;
-	mower.rotation = 270;
+	mower.vel.y = -6;
+	mower.rotation = 0;
 }
 else if (kb.released('w')){
 	mower.vel.y = 0;
@@ -113,28 +121,28 @@ else if (kb.released('w')){
 
 //mower diagonal
 if (kb.pressing('w')&&kb.pressing('d')){
-	mower.rotation = 315;
+	mower.rotation = 45;
 }
 else if(kb.released('w')&&kb.released('d')){
 	mower.rotationSpeed = 0;
 };
 
 if (kb.pressing('w')&&kb.pressing('a')){
-	mower.rotation = 225;
+	mower.rotation = 315;
 }
 else if(kb.released('w')&&kb.released('a')){
 	mower.rotationSpeed = 0;
 };
 
 if (kb.pressing('s')&&kb.pressing('d')){
-	mower.rotation = 45;
+	mower.rotation = 135;
 }
 else if(kb.released('s')&&kb.released('d')){
 	mower.rotationSpeed = 0;
 };
 
 if (kb.pressing('s')&&kb.pressing('a')){
-	mower.rotation = 135;
+	mower.rotation = 225;
 }
 else if(kb.released('s')&&kb.released('a')){
 	mower.rotationSpeed = 0;
